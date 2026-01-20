b1f21e211b996642786c43b6911f3c7e5999f28f
trungvu260279-ui <trungvu.260279@gmail.com>
2026-01-20 20:57:15 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Maze from './components/Maze';
+import DeadlineIntro from './components/DeadlineIntro'; // Import component mới
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
   return (
     <div className="app-container">
-      <Maze />
+      {!gameStarted ? (
+        // Hiển thị Intro deadline trước
+        <DeadlineIntro onStart={() => setGameStarted(true)} />
+      ) : (
+        // Vào game chính
+        <Maze />
+      )}
     </div>
   );
 }
